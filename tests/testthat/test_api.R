c0f6ffbb6f9cdcffcf165de8d2e178f131ee401a
Finn Roberts <robe2037@umn.edu>
2022-08-29 14:47:19 -0500

    re-add tests for superfluous revisions

--- tests/testthat/test_api.R
+++ tests/testthat/test_api.R
@@ -570,14 +570,14 @@ test_that("Unused revisions do not alter CPS extract", {
 
 
 test_that("Improper extract revisions throw warnings or errors", {
-  # expect_warning(
-  #   add_to_extract(usa_extract, samples = "us2017b"),
-  #   regexp = "already included"
-  # )
-  # expect_warning(
-  #   remove_from_extract(cps_extract, variables = "RELATE"),
-  #   regexp = "are not included"
-  # )
+  expect_warning(
+    add_to_extract(usa_extract, samples = "us2017b"),
+    regexp = "already included"
+  )
+  expect_warning(
+    remove_from_extract(cps_extract, variables = "RELATE"),
+    regexp = "are not included"
+  )
   expect_warning(
     remove_from_extract(usa_extract,
                         description = "description",
