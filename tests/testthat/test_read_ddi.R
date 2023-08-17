c817dc15bf20dc29da2c5ce560291b97277d7ed4
Finn Roberts <robe2037@umn.edu>
2023-08-17 16:17:07 -0500

    Use fixtures for zip archive tests

--- tests/testthat/test_read_ddi.R
+++ tests/testthat/test_read_ddi.R
@@ -21,61 +21,34 @@ test_that("We ignore layer selection for direct .xml files", {
   expect_identical(ddi1, ddi2)
 })
 
-test_that("Can read ddi from zip archives and directories", {
-  xml1 <- xml2::read_xml(ipums_example("cps_00157.xml"))
-  xml2 <- xml2::read_xml(ipums_example("cps_00159.xml"))
-
-  tmp <- tempfile()
-
-  on.exit(unlink(tmp, recursive = TRUE), add = TRUE, after = FALSE)
-
-  dir.create(tmp)
-
-  xml2::write_xml(xml1, file.path(tmp, "xml1.xml"))
-  xml2::write_xml(xml2, file.path(tmp, "xml2.xml"))
+test_that("Can read ddi from zip archives", {
+  zipped_ddi <- file.path(vcr::vcr_test_path("fixtures"), "zipped_ddi.zip")
 
   expect_error(
-    read_ipums_ddi(tmp),
+    read_ipums_ddi(zipped_ddi),
     "Multiple files found"
   )
 
   expect_error(
-    read_ipums_ddi(tmp, file_select = contains("not-in-file")),
+    read_ipums_ddi(zipped_ddi, file_select = contains("not-in-file")),
     "The provided `file_select` did not select any of the available files"
   )
 
   expect_identical(
-    read_ipums_ddi(tmp, file_select = 1)$var_info,
+    read_ipums_ddi(zipped_ddi, file_select = 1)$var_info,
     read_ipums_ddi(ipums_example("cps_00157.xml"))$var_info
   )
   expect_identical(
-    read_ipums_ddi(tmp, file_select = 2)$var_info,
+    read_ipums_ddi(zipped_ddi, file_select = 2)$var_info,
     read_ipums_ddi(ipums_example("cps_00159.xml"))$var_info
   )
 
-  zip(
-    file.path(tmp, "test.zip"),
-    c(file.path(tmp, "xml1.xml"), file.path(tmp, "xml2.xml")),
-    flags = "-q"
-  )
-
-  expect_error(
-    read_ipums_ddi(file.path(tmp, "test.zip")),
-    "Multiple files found"
-  )
-
   expect_identical(
-    read_ipums_ddi(
-      file.path(tmp, "test.zip"),
-      file_select = contains("xml1")
-    )$var_info,
+    read_ipums_ddi(zipped_ddi, file_select = contains("xml1"))$var_info,
     read_ipums_ddi(ipums_example("cps_00157.xml"))$var_info
   )
   expect_identical(
-    read_ipums_ddi(
-      file.path(tmp, "test.zip"),
-      file_select = contains("xml2")
-    )$var_info,
+    read_ipums_ddi(zipped_ddi, file_select = contains("xml2"))$var_info,
     read_ipums_ddi(ipums_example("cps_00159.xml"))$var_info
   )
 })
