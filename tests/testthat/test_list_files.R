1ea38d93ccf5e749238c101481b95e6c7e68871f
Finn Roberts <robe2037@umn.edu>
2025-02-20 11:38:04 -0500

    Show codebooks in `ipums_list_files()`

--- tests/testthat/test_list_files.R
+++ tests/testthat/test_list_files.R
@@ -1,22 +1,28 @@
-nhgis_csv <- ipums_example("nhgis0972_csv.zip")
-nhgis_shp <- ipums_example("nhgis0972_shape_small.zip")
-
 test_that("Can list files from zipped data extract", {
-  if (!file.exists(nhgis_csv)) {
-    skip("Couldn't find nhgis csv.")
-  }
+  file <- ipums_example("nhgis0972_csv.zip")
 
-  all_files <- ipums_list_files(nhgis_csv)
-  expect_equal(all_files$type, "data")
-  expect_equal(all_files$file, "nhgis0972_csv/nhgis0972_ds135_1990_pmsa.csv")
+  all_files <- ipums_list_files(file)
+  d_files <- ipums_list_files(file, types = "data")
+  cb_file <- ipums_list_files(file, file_select = contains("pmsa_codebook"))
+
+  expect_equal(all_files$type, c("data", "codebook"))
+  expect_equal(
+    all_files$file,
+    c("nhgis0972_csv/nhgis0972_ds135_1990_pmsa.csv",
+      "nhgis0972_csv/nhgis0972_ds135_1990_pmsa_codebook.txt")
+  )
+  expect_equal(d_files$type, "data")
+  expect_equal(d_files$file, "nhgis0972_csv/nhgis0972_ds135_1990_pmsa.csv")
+  expect_equal(
+    cb_file$file,
+    "nhgis0972_csv/nhgis0972_ds135_1990_pmsa_codebook.txt"
+  )
+  expect_equal(nrow(ipums_list_files(file, file_select = "foo")), 0)
 })
 
 test_that("Can list files from zipped shapefile", {
-  if (!file.exists(nhgis_shp)) {
-    skip("Couldn't find nhgis shape")
-  }
-
-  all_files <- ipums_list_files(nhgis_shp)
+  file <- ipums_example("nhgis0972_shape_small.zip")
+  all_files <- ipums_list_files(file)
   expect_equal(all_files$type, "shape")
   expect_equal(
     all_files$file,
